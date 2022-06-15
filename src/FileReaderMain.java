4ecb6b4a61b867e1f1e80f527770ecc7779fcdff
MadScientist-001 <rami.kh04@gmail.com>
2022-06-16 02:57:25 +0300

    deleted test classes

--- src/FileReaderMain.java
+++
@@ -1,6 +0,0 @@
-public class FileReaderMain {
-    public static void main(String[] args) {
-        System.out.println(FileReader.readResearcherProfiles("researchers.txt").printAlphabetical());
-
-    }
-}
--- src/GraphMain.java
+++
@@ -1,7 +0,0 @@
-public class GraphMain {
-    public static void main(String[] args) {
-        //BST tree = FileReader.readResearcherProfiles("researchers.txt");
-        //Graph graph = new Graph("EdgeList.txt", tree);
-        //System.out.println(graph.findInfluencer("Archambault"));
-    }
-}
